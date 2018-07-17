eea4d9297c446c504cbbe171a3f81d6a30fbb1e2
Kevin Schaefer <kevin.schaefer13@gmail.com>
2018-07-17 23:27:12 +0200

    initial commit

---
+++ backend/start/routes.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.0/routing
+|
+*/
+
+const Route = use('Route')
+
+Route.get('/', ({ request }) => {
+  return { greeting: 'Hello world in JSON' }
+})
+
+Route.post('/signup', 'UserController.signup')
+Route.post('/login', 'UserController.login')
+
+Route.group(() => {
+  Route.get('/me', 'UserController.me')
+  Route.put('/update_profile', 'UserController.updateProfile')
+  Route.put('/change_password', 'UserController.changePassword')
+})
+  .prefix('account')
+  .middleware(['auth:jwt'])
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Overview
+Digital book library for learning purposes and maybe use it at home

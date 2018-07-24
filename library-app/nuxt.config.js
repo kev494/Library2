f0d3c542e77a2b8d923672a7f7dc969a6f0671b7
Kevin Schaefer <kevin.schaefer13@gmail.com>
2018-07-24 21:30:38 +0200

    first configurations etc

--- library-app/nuxt.config.js
+++ library-app/nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'library-app',
+    title: 'Fancy Library',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -13,6 +13,20 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+
+
+  modules: [
+    '@nuxtjs/axios',
+  ],
+
+  axios: {
+    // proxyHeaders: false
+    baseURL: 'http://127.0.0.1:3333'
+  },
+
+  plugins: [
+    '~/plugins/element-ui'
+  ],
   /*
   ** Customize the progress bar color
   */
---
+++ library-app/pages/books/index.vue
@@ -0,0 +1,6 @@
+<template>
+<div>
+    <h1>Books</h1>
+    
+</div>
+</template>

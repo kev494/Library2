c8d7fd65f11b0f7cea633c8ed0a123758b51030f
Kevin Schaefer <kevin.schaefer13@gmail.com>
2019-06-06 22:58:51 +0200

    vuex store for show

--- library-app/pages/books/_id/index.vue
+++ library-app/pages/books/_id/index.vue
@@ -7,10 +7,18 @@
 <script>
 import axios from 'axios'
 export default {
-    async asyncData({params}) {
-        const {data} = await axios.get(`http://127.0.0.1:3333/books/${params.id}`)
-        return { book: data.data}
+    //async asyncData({params}) {
+    //    const {data} = await axios.get(`http://127.0.0.1:3333/books/${params.id}`)
+    //    return { book: data.data}
+    //}
+    async fetch({store, params}) {
+      await store.dispatch('books/show', {id: params.id})
+  },
+  computed: {
+    book() {
+      return this.$store.state.books.book.data
     }
+   }
 }
 </script>
 
--- library-app/store/books.js
+++ library-app/store/books.js
@@ -1,10 +1,14 @@
 export const state = () => ({
-    list: []
+    list: [],
+    book: {}
   })
 
 export const mutations = {
   set(state, books) {
     state.list = books
+  },
+  getBook(state, book) {
+    state.book = book
   }
 }
 
@@ -14,6 +18,14 @@ export const actions = {
       .then((res) => {
         if (res.status === 200) {
           commit('set', res.data)
+        }
+      })
+  },
+  async show({commit}, params) {
+    await this.$axios.get(`/books/${params.id}`)
+      .then((res) => {
+        if (res.status === 200) {
+          commit('getBook', res.data)
         }
       })
   }
